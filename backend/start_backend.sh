f078297e4ed8804b3a58df534f06e052811453d1
mrSoftIce <mr.softice@gmail.com>
2025-11-07 12:23:11 +0400

    fix: problems with staticcollection

--- backend/start_backend.sh
+++ backend/start_backend.sh
@@ -6,5 +6,6 @@ python manage.py migrate
 
 # Collect static files
 python manage.py collectstatic --no-input
+cp -r /app/collected_static/. /backend_static/static/
 
 gunicorn --bind 0:8000 backend.wsgi
