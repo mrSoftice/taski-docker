8f792dfc8a3718836e9b1069b78dcad093a520c1
mrSoftIce <mr.softice@gmail.com>
2025-11-07 12:34:53 +0400

    fix2: problem with static collection

--- collect_static_production.sh
+++ collect_static_production.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 echo "Collect static files..."
-docker compose -f docker-compose.production.yml exec backend python manage.py collectstatic
+docker compose -f docker-compose.production.yml exec backend python manage.py collectstatic --no-input
 
 echo "Copy them to volume..."
 docker compose -f docker-compose.production.yml exec backend sh -c "cp -r /app/collected_static/. /backend_static/static/"
-
