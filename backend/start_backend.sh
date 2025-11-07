7395cd8e855134b0a0aa7335da03c2e3ca45af17
mrSoftIce <mr.softice@gmail.com>
2025-11-07 11:55:12 +0400

    collect static befor backend starts

--- backend/start_backend.sh
+++ backend/start_backend.sh
@@ -2,7 +2,9 @@
 
 echo "Starting taski backend..."
 
-python manage.py migrate 
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --no-input
 
 gunicorn --bind 0:8000 backend.wsgi
-
